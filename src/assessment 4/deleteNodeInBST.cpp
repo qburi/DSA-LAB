d694d971511ccab8dcad5e787fa31f219da85201
qburi <divyeshbalu@gmail.com>
2025-09-20 22:01:30 +0530

    no clue what changed in deleteNodeInBST.cpp

---
+++ src/assessment 4/deleteNodeInBST.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left = nullptr, *right = nullptr;
+    TreeNode(int v) : val(v) {}
+};
+
+TreeNode* insert(TreeNode* root, int val) {
+    if (!root) return new TreeNode(val);
+    if (val < root->val) root->left = insert(root->left, val);
+    else root->right = insert(root->right, val);
+    return root;
+}
+
+TreeNode* findMax(TreeNode* node) {
+    while (node->right) node = node->right;
+    return node;
+}
+
+TreeNode* deleteNode(TreeNode* root, int val, bool& found) {
+    if (!root) return nullptr;
+
+    if (val < root->val) {
+        root->left = deleteNode(root->left, val, found);
+    } else if (val > root->val) {
+        root->right = deleteNode(root->right, val, found);
+    } else {
+        found = true;
+        if (!root->left || !root->right) {
+            return root->left ? root->left : root->right;
+        }
+        TreeNode* pred = findMax(root->left);
+        root->val = pred->val;
+        bool dummy = true;
+        root->left = deleteNode(root->left, pred->val, dummy);
+    }
+    return root;
+}
+
+void preorder(TreeNode* root) {
+    if (!root) return;
+    cout << root->val << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main() {
+    int n, r, val;
+    cin >> n;
+    TreeNode* root = nullptr;
+    for (int i = 0; i < n; ++i) {
+        cin >> val;
+        root = insert(root, val);
+    }
+
+    cin >> r;
+
+    for (int i = 0; i < r; ++i) {
+        cin >> val;
+        bool foundNode = false;
+        root = deleteNode(root, val, foundNode);
+        if (!foundNode) {
+            cout << "No such element is in the tree" << endl;
+        }
+    }
+
+    preorder(root);
+    cout << endl;
+
+    return 0;
+}
