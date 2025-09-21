33bc8a1f6daed7aa281f2a343e232654b988106b
qburi <divyeshbalu@gmail.com>
2025-09-21 16:49:35 +0530

    implemented findKthMinimumElementinBST.cpp

---
+++ src/treesLore/findKthMinimumElementinBST.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+
+class Solution {
+public:
+    int minVal;
+
+    void dfs(TreeNode* root, int& k) {
+        if (root == nullptr || k <= 0) return;
+
+        dfs(root->left, k);
+        k--;
+        if (k == 0) {
+            minVal = root->val;
+            return;
+        }
+        dfs(root->right, k);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        if (root == nullptr) return 0;
+        dfs(root, k);
+        return minVal;
+    }
+};
+
+
+
+int main() {
+    return 0;
+}
